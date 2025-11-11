455ed8e89d305721a711a19596b90d2362ebad30
XantSpace <heis.buike99@gmail.com>
2025-11-11 09:55:41 +0100

    Python assihnment

---
+++ app.py
@@ -0,0 +1,81 @@
+# This is FIRST ASSIGNMENT
+print("My First assignment")
+# This is SECTION A
+word1 = 'Hello, '
+word2 = "welcome "
+word3 = """to python class"""
+print(f"{word1}{word2}{word3}")
+
+sentence = "Python is fun to learn"
+print(sentence)
+
+message = """
+ Python is powerful.
+ It is easy to learn.
+ It is loved by developers
+"""
+print(message)
+
+# This is SECTION B
+text = "PYTHON"
+print(text[0])
+print(text[2])
+print(text[5])
+
+print("###")
+
+language = "PYTHON"
+for language in language:
+    print(language)
+
+print("###")
+# This is SECTION C
+fruit = "BANANA"
+print(len(fruit))
+
+word = "Learning Python is cool"
+if "Python" in word:
+    print("Yes! 'Python' is found.")
+if "Java" in word:
+    print("Yes! 'Java' is found.")
+else:
+    print("No! Java is not Available")
+
+# BONUS ASSIGNMENT
+msg = "Coding is fun"
+count = 0
+for n in msg:
+    if n == "n":
+        count += 1
+print(count)
+
+poem = """
+Roses are red,
+Violets are blue,
+Python is rare,
+And that's why it is cool.
+
+"""
+print(poem.upper())
+
+# ## This is the second assignment
+print("This the second assignment")
+# #Section A Variables
+
+name = "Ada"
+age = 18
+school = "Bright Future Academy"
+print(f"My name is {name} and I am {age} years old. and i attend {school}")
+
+country = "Nigeria"
+capital = "Abuja"
+print(f"The capital of {country} is {capital}")
+
+first_name = "John"
+middle_name = "Paul"
+last_name = "Okoro"
+print(f"{first_name} {middle_name} {last_name}")
+
+food = "Rice"
+drink = "Juice"
+print(f"I love eating {food} and {drink}")
