1146e73ec7be1eaa71bcc25ff14af79ba3ebbb5a
Pleasant <johnsonpleasant02@gmail.com>
2025-11-14 14:16:44 +0100

    updated

--- CLASS2.py
+++ CLASS2.py
@@ -35,3 +35,54 @@ even_numbers = (2,4,6,8,10)
 print(even_numbers)
 multi_dat_li = ("str", 10, 0.1, ["Toyota", "Honda"], False, {"key":"value"})
 print(multi_dat_li)
+print(type(multi_dat_li))
+print(multi_dat_li[0])
+print(multi_dat_li[3])
+
+
+fruits = ["apple", "banana", "cherry", "orange"]
+fruits[3] = "Mango"
+fruits = tuple(fruits)
+print(fruits)
+
+person = ("Pleasant", 13, "bende")
+name, age, place = person
+print(f"My name is {name} and i am {age} years old and i am from {place}.")
+
+#Create a tuple of your 5 favourite foods
+#print the first, middle, and last items
+#convert it to a list and change one food
+#convert it back to a tuple
+
+foods = ("Spagetti", "Jollof-Rice", "Yam Porridge", "Egg Sauce")
+print(foods[0])
+print(foods[1])
+print(foods[3])
+z = list(foods)
+z[2] = "Fried Rice"
+z = tuple(z)
+print(z)
+
+#back to lessons
+#DICTIONARIES
+#it follows a principle
+#keys are unique whilst values can be changed
+student = {
+    "name" : "Pleasant",
+    "age"  : 13,
+    "Location" : "Bende",
+    "class" : "100L"
+
+}
+#accessing values in dictionaries
+print(student["name"])
+print(student.get("name"))
+
+#changing values in a dictionary
+student["age"] = 100
+print(student["age"])
+
+#to add keys and values to an already existing dictionary
+student["School"] = "UNICAL"
+print(student)
+
